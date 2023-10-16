021b7b5b5a955362ea66c5377aa047958fc74bbc
Amanda Arruda <amanda.arruda@unesp.br>
2023-10-16 19:21:19 -0300

    atualizando error message

--- src/controladores/usuario.js
+++ src/controladores/usuario.js
@@ -7,28 +7,51 @@ require('dotenv').config();
 const cadastrarUsuario = async (req, res) => {
     const { nome, email, senha } = req.body;
 
-    const camposObrigatorios = joi.object({ nome: joi.string().required(), email: joi.string().email().required(), senha: joi.string().min(5).required(), });
+    const camposObrigatorios = joi.object({
+        nome: joi.string().required().messages({
+            "any.required": "O campo nome é obrigatório",
+            "string.empty": "O campo nome é obrigatório",
+        }),
+        email: joi.string().email().required().messages({
+            "any.required": "O campo email é obrigatório",
+            "string.email": "Digite um email válido",
+            "string.empty": "O campo email é obrigatório",
+        }),
+        senha: joi.string().required().messages({
+            "any.required": "O campo senha é obrigatório",
+            "string.empty": "O campo senha é obrigatório",
+        }),
+    });
 
 
     try {
         await camposObrigatorios.validateAsync(req.body);
     } catch (error) {
-        return res.status(400).json({ mensagem: 'Os campos nome, email e senha são obrigatórios!' });
+        return res.status(400)
+            .json({ mensagem: error.message });
     }
 
     try {
         const conferirEmail = await knex("usuarios").where({ email });
 
         if (conferirEmail.length > 0) {
-            return res.status(400).json({ mensagem: 'Esse e-mail já está cadastrado.' });
+            return res.status(400)
+                .json({ mensagem: 'Esse e-mail já está cadastrado.' });
         }
 
         const criptografarSenha = await bcrypt.hash(senha, 10);
 
-        const preencher = await knex("usuarios").insert({ nome, email, senha: criptografarSenha, });
+        const preencher = await knex("usuarios").insert({
+            nome,
+            email,
+            senha: criptografarSenha,
+        });
 
         if (preencher.rowCount === 0) {
-            return res.status(400).json({ mensagem: 'Usuário não pôde ser cadastrado! Preencha todos os campos obrigatórios!!' });
+            return res.status(400).json({
+                mensagem:
+                    'Usuário não pôde ser cadastrado! Preencha todos os campos obrigatórios!!'
+            });
         }
 
         const novoUsuario = await knex("usuarios").where({ email }).first();
