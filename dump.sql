31a6a307caf0906c044ef7f2f6d63b935c706ae7
larissaangioni <larissaangioni@gmail.com>
2023-10-18 09:41:19 -0300

    dump, arquivos controladores

--- dump.sql
+++ dump.sql
@@ -21,3 +21,24 @@ VALUES
 ('Moda'),
 ('Bebê'),
 ('Games');
+
+CREATE TABLE produtos (
+id serial primary key,
+descricao varchar(255) not null,
+quantidade_estoque int not null,
+valor int not null,
+categoria_id int REFERENCES categorias(id) not null
+)
+
+CREATE TABLE clientes (
+id serial primary key,
+nome varchar(255) not null,
+email varchar(255) unique not null,
+cpf varchar(15) unique not null,
+cep varchar(10),
+rua varchar(255),
+numero varchar(50),
+bairro varchar(255),
+cidade varchar(255),
+estado varchar(255)
+)
