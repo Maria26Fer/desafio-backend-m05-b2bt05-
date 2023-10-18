b26e34c731d2b7bea196e3a1b961ef55f39f849f
larissaangioni <larissaangioni@gmail.com>
2023-10-18 10:19:50 -0300

    feat: cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+const cors = require("cors");
 const rotas = require("./rotas");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(rotas);
 
